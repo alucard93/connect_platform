efa4e6561c9b3f3caa90a330afe24eb024ac646a
Marcus Vinicius Nascimento Ferreira <dev.mvinicius@gmail.com>
2026-02-01 13:40:10 -0300

    fix: update RouterProvider import and adjust styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router'
+import { RouterProvider } from 'react-router-dom'
 import { router } from './routes' // Importa sua config da pasta routes
-// import './styles/global.css'
+import './styles/main.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
